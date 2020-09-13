c8a13511f5979974970177f618488b02a8ddaa46
Erik Schierboom <erik_schierboom@hotmail.com>
2020-09-13 20:03:00 +0200

    Inline

--- src/commands/check.nim
+++ src/commands/check.nim
@@ -1,29 +1,30 @@
 import options, sequtils, strformat
 import ../arguments, ../exercises
 
-proc missingTestCases(exercise: Exercise): bool =
-  exercise.testCases.missing.len > 0
-
-proc printExerciseStatuses(exercises: seq[Exercise]): void =
-  for exercise in exercises:
-    if exercise.missingTestCases:
-      echo &"[warn] {exercise.slug}"
-
-proc printOverallStatus(exercises: seq[Exercise]): void =
-  if exercises.anyIt(it.missingTestCases):
-    echo "[warn] some exercises are missing test cases"
-  else:
-    echo "All exercises are up-to-date!"
-
 proc findExercises(args: Arguments): seq[Exercise] =
   if args.exercise.isNone:
     result = findExercises()
   else:
     result = findExercises().filterIt(it.slug == args.exercise.get)
 
+proc missingTestCases(exercise: Exercise): bool =
+  exercise.testCases.missing.len > 0
+
+proc check(exercises: seq[Exercise]): void =
+  var missingTestCases = false
+
+  for exercise in exercises:
+    if exercise.missingTestCases:
+      echo &"[warn] {exercise.slug}"
+      missingTestCases = true
+
+  if missingTestCases:
+    echo "[warn] some exercises are missing test cases"
+  else:
+    echo "All exercises are up-to-date!"
+
 proc check*(args: Arguments): void =
   echo "Checking exercises..."
 
   let exercises = findExercises(args)
-  printExerciseStatuses(exercises)
-  printOverallStatus(exercises)
+  check(exercises)
