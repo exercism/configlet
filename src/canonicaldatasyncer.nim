9bba780a24819ca7a36edef46bd4d47c12574cb3
Erik Schierboom <erik_schierboom@hotmail.com>
2020-09-13 18:24:29 +0200

    Extract options handling

--- src/canonicaldatasyncer.nim
+++ src/canonicaldatasyncer.nim
@@ -1,20 +1,15 @@
-import os, strutils, strformat, syncer
+import options, strformat
 
 const NimblePkgVersion {.strdefine}: string = "unknown"
 
-let describeUsage = &"Usage: {extractFileName(getAppFilename())} [check|update]"
-
 echo &"Exercism Canonical Data Syncer v{NimblePkgVersion}"
 
-if paramCount() == 0:
-    quit(describeUsage)
+let opts = parseOptions()
 
-case paramStr(1)
-of "check":
-  echo "check"
-  syncTests()
-of "update":
-  echo "update"
-else:
-  echo &"Invalid option: {paramStr(1)}"
-  quit(describeUsage)
+case opts.command
+of Command.check:
+  echo "Check check"
+of Command.update:
+  echo "Update"
+of Command.format:
+  echo "Format"
