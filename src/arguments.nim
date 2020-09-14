4efa8aff609bfe47ab1d6aa47a84b48bf6ee905d
Erik Schierboom <erik_schierboom@hotmail.com>
2020-09-14 10:46:39 +0200

    Update

--- src/arguments.nim
+++ src/arguments.nim
@@ -4,73 +4,75 @@ type
   Command* = enum
     check, update
 
-  Verbosity* = enum
+  LogLevel* = enum
     quiet, normal, detailed
 
   Arguments* = object
     command*: Command
-    verbosity*: Verbosity
+    logLevel*: LogLevel
     exercise*: Option[string]
+
+  Argument = tuple[short: string, long: string]
 
 const NimblePkgVersion {.strdefine}: string = "unknown"
 
+const ExerciseArgument: Argument = (short: "e", long: "exercise")
+const LogLevelArgument: Argument = (short: "l", long: "loglevel")
+const HelpArgument    : Argument = (short: "h", long: "help")
+const VersionArgument : Argument = (short: "v", long: "version")
+
 proc showHelp() =
   let commandOptions = toSeq(Command).join("|")
-  let verbosityOptions = toSeq(Verbosity).mapIt(&"[{($it)[0]}]{($it)[1..^1]}").join("|")
+  let verbosityOptions = toSeq(LogLevel).mapIt(&"[{($it)[0]}]{($it)[1..^1]}").join("|")
   let applicationName = extractFileName(getAppFilename())
 
   echo &"Usage: {applicationName} [options] {{{commandOptions}}}"
   echo ""
   echo "Options:"
-  echo "  -e, --exercise <slug>       Check/update only this exercise"
-  echo &"  -l, --loglevel <verbosity>  Set the verbosity level: {verbosityOptions}"
-  echo "  -h, --help                  Show CLI usage" 
-  echo "  -v, --version               Display version information"
+  echo &"  -{ExerciseArgument.short}, --{ExerciseArgument.long} <slug>       Check/update only this exercise"
+  echo &"  -{LogLevelArgument.short}, --{LogLevelArgument.long} <verbosity>  Set the log level: {verbosityOptions}"
+  echo &"  -{HelpArgument.short}, --{HelpArgument.long}                  Show CLI usage" 
+  echo &"  -{VersionArgument.short}, --{VersionArgument.long}               Display version information"
 
 proc showVersion() = 
   echo &"Canonical Data Syncer v{NimblePkgVersion}"
 
-var filename: string
-var p = initOptParser("--left --debug:3 -l -r:2")
-
-for kind, key, val in p.getopt():
-  case kind
-  of cmdArgument:
-    filename = key
-  of cmdLongOption, cmdShortOption:
-    case key
-    of "help", "h": showHelp()
-    of "version", "v": showVersion()
-  of cmdEnd: assert(false) # cannot happen
-if filename == "":
-  # no filename has been given, so we show the help
+proc handleFailure: void =
   showHelp()
-
-
-proc noParameters: bool =
-  paramCount() == 0
-
-proc commandParameter: string =
-  paramStr(1)
-
-proc exerciseParameter: string =
-  paramStr(2)
-
-proc parseCommand: Command =
-  try:
-    result = parseEnum[Command](commandParameter())
-  except ValueError:
-    quit(QuitFailure)
-
-proc parseExercise: Option[string] =
-  if paramCount() >= 2:
-    result = some(exerciseParameter())
+  quit(QuitFailure)
 
 proc parseArguments*: Arguments =
-  if noParameters():
-      showHelp()
-      quit(QuitFailure)
+  var command: Option[string]
+  var exercise: Option[string]
+  var logLevel: Option[string]
 
-  # TODO: parse verbosity
-  result.command = parseCommand()
-  result.exercise = parseExercise()
+  var optParser = initOptParser()
+  for kind, key, val in optParser.getopt():
+    case kind
+    of cmdArgument:
+       command = some(key)
+       break
+    of cmdLongOption, cmdShortOption:
+      case key
+      of ExerciseArgument.short, ExerciseArgument.long:
+        exercise = some(key)
+      of LogLevelArgument.short, LogLevelArgument.long:
+        logLevel = some(key)
+      of HelpArgument.short, HelpArgument.long:
+        showHelp()
+        quit(QuitSuccess)
+      of VersionArgument.short, VersionArgument.long:
+        showVersion()
+        quit(QuitSuccess)
+    of cmdEnd: 
+      handleFailure()
+
+  if command.isNone:
+    handleFailure()
+
+  try:
+    result.command = parseEnum[Command](command.get)
+    result.logLevel = parseEnum[LogLevel](logLevel.get($LogLevel.normal))
+    result.exercise = exercise
+  except ValueError:
+    handleFailure()
