1c7da18984dafcdbe570bc033c01913b105bb386
ee7 <45465154+ee7@users.noreply.github.com>
2020-10-09 14:15:08 +0200

    Refactor(cli): Rename `--default` to `--mode`
    Reasons:
    - `-m, --mode <mode>` is more descriptive than `-d, --default <mode>`,
      and fits better with `-o, --verbosity <verbosity>`.
    - We call it `Mode` and `parseMode` internally.

--- src/cli.nim
+++ src/cli.nim
@@ -22,7 +22,7 @@ const NimblePkgVersion {.strdefine.}: string = "unknown"
 
 const ExerciseArgument  : Argument = (short: "e", long: "exercise")
 const CheckArgument     : Argument = (short: "c", long: "check")
-const DefaultArgument   : Argument = (short: "d", long: "default")
+const ModeArgument      : Argument = (short: "m", long: "mode")
 const VerbosityArgument : Argument = (short: "o", long: "verbosity")
 const HelpArgument      : Argument = (short: "h", long: "help")
 const VersionArgument   : Argument = (short: "v", long: "version")
@@ -35,7 +35,7 @@ proc showHelp =
 Options:
   -{ExerciseArgument.short}, --{ExerciseArgument.long} <slug>        Only sync this exercise
   -{CheckArgument.short}, --{CheckArgument.long}                  Check if there are missing tests. Doesn't update the tests. Terminates with a non-zero exit code if one or more tests are missing
-  -{DefaultArgument.short}, --{DefaultArgument.long} <mode>         What to do with missing test cases. Allowed values: c[hoose], i[nclude], e[xclude]
+  -{ModeArgument.short}, --{ModeArgument.long} <mode>            What to do with missing test cases. Allowed values: c[hoose], i[nclude], e[xclude]
   -{VerbosityArgument.short}, --{VerbosityArgument.long} <verbosity>  The verbosity of output. Allowed values: q[uiet], n[ormal], d[etailed]
   -{HelpArgument.short}, --{HelpArgument.long}                   Show this help message and exit
   -{VersionArgument.short}, --{VersionArgument.long}                Show this tool's version information and exit"""
@@ -79,7 +79,7 @@ proc parseArguments*: Arguments =
         result.exercise = some(val)
       of CheckArgument.short, CheckArgument.long:
         result.action = actCheck
-      of DefaultArgument.short, DefaultArgument.long:
+      of ModeArgument.short, ModeArgument.long:
         result.mode = parseMode(val)
       of VerbosityArgument.short, VerbosityArgument.long:
         result.verbosity = parseVerbosity(val)
