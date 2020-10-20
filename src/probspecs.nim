exercism/configlet

commit f502ab6de239899c9137cf78ac05d84ae45fc72c

Make git clone quiet (#49)
Co-authored-by: Erik Schierboom <erik_schierboom@hotmail.com>

src/probspecs.nim
@@ -1,5 +1,5 @@
 import std/[json, options, os, osproc, sequtils, strformat, strutils]
-import cli
+import cli, logger
 
 type
   ProbSpecsRepoExercise = object
@@ -26,7 +26,8 @@ proc initProbSpecsRepo: ProbSpecsRepo =
   result.dir = probSpecsDir()
 
 proc clone(repo: ProbSpecsRepo) =
-  let cmd = &"git clone --depth 1 https://github.com/exercism/problem-specifications.git {repo.dir}"
+  let cmd = &"git clone --quiet --depth 1 https://github.com/exercism/problem-specifications.git {repo.dir}"
+  logDetailed(&"Cloning the problem-specifications repo into {repo.dir}...")
   execCmdException(cmd, "Could not clone problem-specifications repo")
 
 proc grainsWorkaround(repo: ProbSpecsRepo) =

commit 3463eddd104572d0a88b91e9c896455829c0da06

Fix: Show minimal `git clone` output by default (#60)
Commit f502ab6de239 (#49) caused the program to produce no output during
the `git clone` at the default verbosity setting. It was nice to remove
the excessive `git clone` output, but total removal means that the
program can appear to hang on a poor internet connection.

This commit makes the situation a little better for that case. In the
future, we can try to improve the behaviour further.

Fixes: #52

src/probspecs.nim
@@ -27,7 +27,7 @@ proc initProbSpecsRepo: ProbSpecsRepo =
 
 proc clone(repo: ProbSpecsRepo) =
   let cmd = &"git clone --quiet --depth 1 https://github.com/exercism/problem-specifications.git {repo.dir}"
-  logDetailed(&"Cloning the problem-specifications repo into {repo.dir}...")
+  logNormal(&"Cloning the problem-specifications repo into {repo.dir}...")
   execCmdException(cmd, "Could not clone problem-specifications repo")
 
 proc grainsWorkaround(repo: ProbSpecsRepo) =