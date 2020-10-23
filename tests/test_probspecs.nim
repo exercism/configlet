ff29cb1499f197a796db7bc49c8a116ee20da50d
ee7 <45465154+ee7@users.noreply.github.com>
2020-10-23 13:00:03 +0200

    Test(probspecs): Add initial tests
    This commit adds our first tests.
    
    Let's start with some basic checking of `findProbSpecsExercises`, which
    is the main exported proc in `src/probspecs.nim`. We check the default
    behaviour of creating a temporary shallow clone, as well as the
    behaviour of the `--probSpecsDir` option in the simplest case.

---
+++ tests/test_probspecs.nim
@@ -0,0 +1,61 @@
+# This module contains tests for `src/probspecs.nim`
+import std/[json, options, os, osproc, strformat, unittest]
+import cli, probspecs
+
+type
+  ProblemSpecsDir = enum
+    psFresh = "fresh clone"
+    psExisting = "existing dir (simulate the `--probSpecsDir` option)"
+
+proc main =
+  let existingDir = getTempDir() / "test_probspecs_problem-specifications"
+  removeDir(existingDir)
+
+  for ps in ProblemSpecsDir:
+    suite &"findProbSpecsExercises: {ps}":
+      if ps == psExisting:
+        let cmd = "git clone --depth 1 --quiet " &
+                  "https://github.com/exercism/problem-specifications/ " &
+                  existingDir
+        test "can make our own clone for later use as an \"existing dir\"":
+          check:
+            execCmd(cmd) == 0
+
+      let probSpecsDir =
+        case ps
+        of psFresh: none(string)
+        of psExisting: some(existingDir)
+
+      let conf = Conf(probSpecsDir: probSpecsDir)
+      let probSpecsExercises = findProbSpecsExercises(conf)
+
+      test "can return the exercises":
+        check:
+          probSpecsExercises.len >= 116
+
+      test "the first exercise is as expected":
+        let exercise = probSpecsExercises[0]
+
+        check:
+          exercise.slug == "acronym" # The first exercise with canonical data.
+          exercise.testCases.len >= 9 # Tests are never removed.
+
+      test "the first test case is as expected":
+        let firstTestCase = probSpecsExercises[0].testCases[0].json
+        let firstTestCaseExpected = """{
+          "uuid": "1e22cceb-c5e4-4562-9afe-aef07ad1eaf4",
+          "description": "basic",
+          "property": "abbreviate",
+          "input": {
+            "phrase": "Portable Network Graphics"
+          },
+          "expected": "PNG"
+        }""".parseJson()
+
+        check:
+          firstTestCase == firstTestCaseExpected
+
+  removeDir(existingDir)
+
+main()
+{.used.}
