589f8b8bd2ac810fa911801b21126e7945a9ca2f
Erik Schierboom <erik_schierboom@hotmail.com>
2021-01-15 09:45:10 +0100

    fetch-configlet: pass parameters as splat

--- scripts/fetch-configlet.ps1
+++ scripts/fetch-configlet.ps1
@@ -1,11 +1,5 @@
 $ErrorActionPreference = "Stop"
 
-Function DownloadUrl ([string] $FileName, $Headers) {
-    $latestUrl = "https://api.github.com/repos/exercism/configlet-v3/releases/latest"
-    $json = Invoke-RestMethod -Headers $Headers -Uri $latestUrl -MaximumRetryCount 3 -RetryIntervalSec 1 -PreserveAuthorizationOnRedirect
-    $json.assets | Where-Object { $_.browser_download_url -match $FileName } | Select-Object -ExpandProperty browser_download_url
-}
-
 Function Headers {
     If ($env:GITHUB_TOKEN) { @{ Authorization = "Bearer ${env:GITHUB_TOKEN}" } } Else { @{ } }
 }
@@ -14,13 +8,25 @@ Function Arch {
     If ([Environment]::Is64BitOperatingSystem) { "64bit" } Else { "32bit" }
 }
 
-$arch = Arch
 $headers = Headers
+$requestOpts = @{
+  Headers = Headers
+  MaximumRetryCount = 3
+  RetryIntervalSec = 1
+  PreserveAuthorizationOnRedirect = $true
+}
+$arch = Arch
 $fileName = "configlet-windows-$arch.zip"
+
+Function Get-DownloadUrl {
+    $latestUrl = "https://api.github.com/repos/exercism/configlet-v3/releases/latest"
+    $json = Invoke-RestMethod -Uri $latestUrl @requestOpts
+    $json.assets | Where-Object { $_.browser_download_url -match $FileName } | Select-Object -ExpandProperty browser_download_url
+}
+
+$downloadUrl = Get-DownloadUrl
 $outputDirectory = "bin"
 $outputFile = Join-Path -Path $outputDirectory -ChildPath $fileName
-$zipUrl = DownloadUrl -FileName $fileName -Headers $headers
-
-Invoke-WebRequest -Headers $headers -Uri $zipUrl -OutFile $outputFile -MaximumRetryCount 3 -RetryIntervalSec 1 -PreserveAuthorizationOnRedirect
+Invoke-WebRequest -Uri $downloadUrl -OutFile $outputFile @requestOpts
 Expand-Archive $outputFile -DestinationPath $outputDirectory -Force
 Remove-Item -Path $outputFile
