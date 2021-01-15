6f18071afda2c9d2f9a802b0705ca9b693a0428e
Erik Schierboom <erik_schierboom@hotmail.com>
2021-01-15 10:09:16 +0100

    fetch-configlet: Use shared request options

--- scripts/fetch-configlet.ps1
+++ scripts/fetch-configlet.ps1
@@ -9,18 +9,18 @@ Function Arch {
 }
 
 $headers = Headers
-$requestOpts = @{
-  Headers = Headers
-  MaximumRetryCount = 3
-  RetryIntervalSec = 1
-  PreserveAuthorizationOnRedirect = $true
-}
 $arch = Arch
 $fileName = "configlet-windows-$arch.zip"
 
+$requestOpts = @{
+    Headers = $headers
+    MaximumRetryCount = 3
+    RetryIntervalSec = 1
+}
+
 Function Get-DownloadUrl {
     $latestUrl = "https://api.github.com/repos/exercism/configlet-v3/releases/latest"
-    $json = Invoke-RestMethod -Uri $latestUrl @requestOpts
+    $json = Invoke-RestMethod -Uri $latestUrl -PreserveAuthorizationOnRedirect @requestOpts
     $json.assets | Where-Object { $_.browser_download_url -match $FileName } | Select-Object -ExpandProperty browser_download_url
 }
 
