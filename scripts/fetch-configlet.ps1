bb61fd5d026adb4971d9c76955c8e0efeaf4f044
Erik Schierboom <erik_schierboom@hotmail.com>
2021-01-28 09:35:31 +0100

    fetch-configlet: add comments

--- scripts/fetch-configlet.ps1
+++ scripts/fetch-configlet.ps1
@@ -1,3 +1,22 @@
+<#
+.SYNOPSIS
+    Fetch the latest version of configlet.
+.DESCRIPTION
+    Fetch the latest version of configlet. Once completed, the bin/ directory
+    will contain the configlet.exe binary.
+.PARAMETER Confirm
+    Require a confirmation from the user before fetching the configlet binary.
+.EXAMPLE
+    The example below will fetch the latest version of configlet:
+    PS C:\> ./bin/fetch-configlet.ps1
+
+    The example below will fetch the latest version of configlet, but only
+    if the user explicitly gives permission to do so:
+    PS C:\> ./bin/fetch-configlet.ps1 -Confirm
+.NOTES
+    The script should be run from the track's root directory.
+#>
+
 param (
     [Parameter(Mandatory = $false)]
     [Switch]$Confirm
@@ -5,9 +24,12 @@ param (
 
 $ErrorActionPreference = "Stop"
 
+# Download the script that will fetch the configlet.exe binary
 $scriptUrl = "https://raw.githubusercontent.com/exercism/configlet/main/scripts/fetch-configlet-script.ps1"
 $script = Invoke-WebRequest -Uri "${scriptUrl}" -MaximumRetryCount 3 -RetryIntervalSec 1
 
+# Require confirmation from the user before executing the fetch script
+# if the -Confirm switch argument was passed
 if ($Confirm.IsPresent) {
     $choice = Read-Host "${script}`n`nDo you want the execute the above script (y/n)?"
     if ("${choice}" -ne "y" -and "${choice}" -ne "Y") {
@@ -15,4 +37,5 @@ if ($Confirm.IsPresent) {
     }
 }
 
+# Execute the fetch script
 Invoke-Expression "${script}"
