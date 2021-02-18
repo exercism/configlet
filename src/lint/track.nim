4627a76f241b96cb9b66a0377d0baeabd505d125
Erik Schierboom <erik_schierboom@hotmail.com>
2021-02-18 13:54:06 +0100

    lint: validate basic property of track config.json file
    The properties validated are:
    
    - language: non-empty string (required)
    - slug: non-empty string (required)
    - blurb: non-empty string (required)
    - active: boolean (required)
    - version: integer (required)

--- src/lint/track.nim
+++ src/lint/track.nim
@@ -1,13 +1,27 @@
 import std/[json, os, terminal]
-import ".."/[helpers]
+import ".."/helpers
+import "."/validators
 
+proc isValidTrackConfig(data: JsonNode, path: string): bool =
+  if isObject(data, "root", path):
+    result = true
+    checkString("language")
+    checkString("slug")
+    checkString("blurb")
+    checkBoolean("active")
+    checkInteger("version")
+    
 proc isTrackConfigValid*(trackDir: string): bool =
   result = true
   let configJsonPath = trackDir / "config.json"
   if fileExists(configJsonPath):
-    try:
-      let j = parseFile(configJsonPath)
-    except:
-      writeError("JSON parsing error", getCurrentExceptionMsg())
+    let j = 
+      try:
+        parseFile(configJsonPath)
+      except:
+        writeError("JSON parsing error", getCurrentExceptionMsg())
+        return
+    if not isValidTrackConfig(j, configJsonPath):
+        result = false
   else:
     writeError("Missing file", configJsonPath)
