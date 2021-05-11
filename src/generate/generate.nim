6a6136b21ede3a6ff31f04fb0a1acdf13a22477a
ee7 <45465154+ee7@users.noreply.github.com>
2021-05-11 13:26:15 +0200

    refactor: add comments

--- src/generate/generate.nim
+++ src/generate/generate.nim
@@ -11,6 +11,8 @@ proc writeError(description: string, path: Path) =
   stderr.write "\n"
 
 proc conceptIntroduction(trackDir: Path, slug: string, templatePath: Path): string =
+  ## Returns the contents of the `introduction.md` file for a `slug`, but
+  ## without any top-level heading, and without any leading/trailing whitespace.
   let conceptDir = trackDir / "concepts" / slug
   if dirExists(conceptDir):
     let path = conceptDir / "introduction.md"
@@ -30,11 +32,16 @@ proc conceptIntroduction(trackDir: Path, slug: string, templatePath: Path): stri
     quit(1)
 
 proc generateIntroduction(trackDir: Path, templatePath: Path): string =
+  ## Reads the file at `templatePath` and returns the content of the
+  ## corresponding `introduction.md` file.
   let content = readFile(templatePath)
 
   var idx = 0
   while idx < content.len:
     var conceptSlug = ""
+    # Here, we implement the syntax for a placeholder as %{concept:some-slug}
+    # where we allow spaces/tabs after the opening brace, around the
+    # colon, and before the closing brace. The slug must be in kebab-case.
     if scanp(content, idx,
             "%{", *{' ', '\t'}, "concept", *{' ', '\t'}, ':', *{' ', '\t'},
             +{'a'..'z', '-'} -> conceptSlug.add($_), *{' ', '\t'}, '}'):
@@ -44,6 +51,8 @@ proc generateIntroduction(trackDir: Path, templatePath: Path): string =
       inc idx
 
 proc generate*(conf: Conf) =
+  ## For every Concept Exercise in `conf.trackDir` with an `introduction.md.tpl`
+  ## file, write the corresponding `introduction.md` file.
   let trackDir = Path(conf.trackDir)
 
   let conceptExercisesDir = trackDir / "exercises" / "concept"
