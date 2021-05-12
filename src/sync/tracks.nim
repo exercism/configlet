8db721ffb1e3752b13995df4a60544313dfac549
ee7 <45465154+ee7@users.noreply.github.com>
2021-05-12 08:37:28 +0200

    sync(tracks): improve some error messages (#321)
    This commit improves the error message when using `configlet sync` in
    these situations:
    - the track `config.json` file lacks the `exercises` key
    - the given track root directory lacks a `config.json` file

--- src/sync/tracks.nim
+++ src/sync/tracks.nim
@@ -29,18 +29,28 @@ func testsFile*(exercise: TrackExercise): string =
   PracticeExercisePath("").testsFile()
 
 proc getPracticeExercisePaths(trackDir: TrackDir): seq[PracticeExercisePath] =
-  let config = json.parseFile(trackDir / "config.json")["exercises"]
+  let configFile = trackDir / "config.json"
+  if fileExists(configFile):
+    let config = json.parseFile(configFile)
+    if config.hasKey("exercises"):
+      let exercises = config["exercises"]
+      if exercises.hasKey("practice"):
+        let practiceExercises = exercises["practice"]
+        result = newSeqOfCap[PracticeExercisePath](practiceExercises.len)
 
-  if config.hasKey("practice"):
-    let practiceExercises = config["practice"]
-    result = newSeqOfCap[PracticeExercisePath](practiceExercises.len)
-
-    for exercise in practiceExercises:
-      if exercise.hasKey("slug"):
-        if exercise["slug"].kind == JString:
-          let slug = exercise["slug"].getStr()
-          let path = trackDir / "exercises" / "practice" / slug
-          result.add PracticeExercisePath(path)
+        for exercise in practiceExercises:
+          if exercise.hasKey("slug"):
+            if exercise["slug"].kind == JString:
+              let slug = exercise["slug"].getStr()
+              let path = trackDir / "exercises" / "practice" / slug
+              result.add PracticeExercisePath(path)
+    else:
+      stderr.writeLine "Error: file does not have an `exercises` key:\n" &
+                       configFile
+      quit(1)
+  else:
+    stderr.writeLine "Error: file does not exist:\n" & configFile
+    quit(1)
 
 proc initTrackExerciseTests(exercisePath: PracticeExercisePath): TrackExerciseTests =
   let testsFile = testsFile(exercisePath)
