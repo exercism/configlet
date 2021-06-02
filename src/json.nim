bda5f59edb828749b68d6e4d3cbfa917ca347cfc
ee7 <45465154+ee7@users.noreply.github.com>
2021-06-02 23:19:14 +0200

    json: migrate copyright notice

--- src/json.nim
+++ src/json.nim
@@ -1,11 +1,28 @@
-#
-#
-#            Nim's Runtime Library
-#        (c) Copyright 2015 Andreas Rumpf, Dominik Picheta
-#
-#    See the file "copying.txt", included in this
-#    distribution, for details about the copyright.
-#
+# This file is minimally adapted from the Nim standard library:
+# https://github.com/nim-lang/Nim/blob/82561f31eb8e/lib/pure/json.nim
+# The standard library version is lenient: it silently allows a trailing comma.
+# The below version is stricter: it raises a `JsonParsingError` for a trailing
+# comma.
+
+# Copyright (c) 2015 Andreas Rumpf, Dominik Picheta
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 
 ## This module implements a simple high performance `JSON`:idx:
 ## parser. JSON (JavaScript Object Notation) is a lightweight
