78ff71354256c01c7b80e2b7096f71119eb9290b
ee7 <45465154+ee7@users.noreply.github.com>
2021-06-19 20:46:14 +0200

    sync(check): adapt for `--tests`

--- src/sync/check.nim
+++ src/sync/check.nim
@@ -2,27 +2,32 @@ import std/[sets, strformat]
 import ".."/[cli, logger]
 import "."/exercises
 
+proc checkTests(exercise: Exercise, seenMissingTests: var bool) =
+  let numMissing = exercise.tests.missing.len
+  let wording = if numMissing == 1: "test case" else: "test cases"
+
+  case exercise.status()
+  of exOutOfSync:
+    seenMissingTests = true
+    logNormal(&"[warn] {exercise.slug}: missing {numMissing} {wording}")
+    for testCase in exercise.testCases:
+      if testCase.uuid in exercise.tests.missing:
+        logNormal(&"       - {testCase.description} ({testCase.uuid})")
+  of exInSync:
+    logDetailed(&"[skip] {exercise.slug}: up-to-date")
+  of exNoCanonicalData:
+    logDetailed(&"[skip] {exercise.slug}: does not have canonical data")
+
 proc check*(conf: Conf) =
   logNormal("Checking exercises...")
 
-  var hasOutOfSync = false
+  var seenMissingTests = false
 
   for exercise in findExercises(conf):
-    let numMissing = exercise.tests.missing.len
-    let wording = if numMissing == 1: "test case" else: "test cases"
-    case exercise.status()
-    of exOutOfSync:
-      hasOutOfSync = true
-      logNormal(&"[warn] {exercise.slug}: missing {numMissing} {wording}")
-      for testCase in exercise.testCases:
-        if testCase.uuid in exercise.tests.missing:
-          logNormal(&"       - {testCase.description} ({testCase.uuid})")
-    of exInSync:
-      logDetailed(&"[skip] {exercise.slug}: up-to-date")
-    of exNoCanonicalData:
-      logDetailed(&"[skip] {exercise.slug}: does not have canonical data")
+    if skTests in conf.action.scope:
+      checkTests(exercise, seenMissingTests)
 
-  if hasOutOfSync:
+  if seenMissingTests:
     logNormal("[warn] some exercises are missing test cases")
     quit(QuitFailure)
   else:
