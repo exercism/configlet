8c10570f0e7680b7fa1fec4d13c8ed96244e3698
ee7 <45465154+ee7@users.noreply.github.com>
2021-09-08 14:43:34 +0200

    lint(track_config): make `scanf` pattern stricter
    Require that the `jsony` error message ends with the integer.

--- src/lint/track_config.nim
+++ src/lint/track_config.nim
@@ -258,7 +258,7 @@ proc tidyJsonyErrorMsg(trackConfigContents: string): string =
   var offset = -1
   # See https://github.com/treeform/jsony/blob/33c3daa/src/jsony.nim#L25-L27
   result.add(
-    if jsonyMsg.scanf("$* At offset: $i", jsonyMsgStart, offset):
+    if jsonyMsg.scanf("$* At offset: $i$.", jsonyMsgStart, offset):
       let (line, col) = toLineAndCol(trackConfigContents, offset)
       &"({line}, {col}): {jsonyMsgStart}"
     else:
