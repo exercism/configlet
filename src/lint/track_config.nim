exercism/configlet

commit e732894895f8c57c3631cb243ca10e349bf687b7

lint(track_config): check exercise directory names are in config (#426)
With this commit, `configlet lint` now produces an error if there is
a subdirectory in the `exercises/concept` directory or
`exercises/practice` directory of a track that does not have a
corresponding entry in the track-level `config.json` file.

src/lint/track_config.nim
@@ -1,4 +1,4 @@
-import std/[json, sets, strformat, strscans, strutils, tables]
+import std/[json, os, sets, strformat, strscans, strutils, tables]
 import pkg/jsony
 import ".."/[cli, helpers]
 import "."/validators
@@ -669,6 +669,44 @@ proc satisfiesSecondPass(trackConfigContents: string; path: Path): bool =
   checkExercisesPCP(practiceExercises, result, path)
   checkExerciseSlugsAndForegone(exercises, result, path)
 
+proc getExerciseSlugs(data: JsonNode; k: string): HashSet[string] =
+  result = initHashSet[string]()
+  if data.kind != JObject or "exercises" notin data:
+    return
+
+  let exercises = data["exercises"]
+
+  if exercises.kind != JObject or k notin exercises or exercises[k].kind != JArray:
+    return
+
+  for exercise in exercises[k]:
+    if exercise.kind == JObject:
+      if exercise.hasKey("slug"):
+        let slug = exercise["slug"]
+        if slug.kind == JString:
+          let slugStr = slug.getStr()
+          if slugStr.len > 0:
+            result.incl slugStr
+
+proc checkExerciseDirsAreInTrackConfig(trackDir: Path; data: JsonNode;
+                                       b: var bool; path: Path) =
+  ## Sets `b` to `false` if there is an exercise directory that is
+  ## not an exercise `slug` in `data`.
+  for exerciseKind in ["concept", "practice"]:
+    let exerciseSlugs = getExerciseSlugs(data, exerciseKind)
+    if exerciseSlugs.len > 0:
+      let exercisesDir = trackDir / "exercises" / exerciseKind
+      if dirExists(exercisesDir):
+        for exerciseDir in getSortedSubdirs(exercisesDir):
+          let dirSlug = lastPathPart(exerciseDir.string)
+          if dirSlug notin exerciseSlugs:
+            let msg = &"{q $exercisesDir} contains a directory named {q dirSlug}, " &
+                      &"which is not a `slug` in the array of {exerciseKind} " &
+                       "exercises. Please add the exercise to that array. " &
+                       "If the exercise is not ready to be shown on the " &
+                       "website, please set its `status` value to \"wip\""
+            b.setFalseAndPrint(msg, path)
+
 proc isTrackConfigValid*(trackDir: Path): bool =
   result = true
   let trackConfigPath = trackDir / "config.json"
@@ -681,3 +719,5 @@ proc isTrackConfigValid*(trackDir: Path): bool =
   if result:
     let trackConfigContents = readFile(trackConfigPath)
     result = satisfiesSecondPass(trackConfigContents, trackConfigPath)
+
+  checkExerciseDirsAreInTrackConfig(trackDir, j, result, trackConfigPath)

commit 03847053d9821e2873abdaaa64c732b807ab9e0f

lint(track_config): fix error for missing track config
With the most recent configlet release (4.0.0-alpha.31), running
`configlet lint` using a directory that lacks a `config.json` file would
produce:

    The lint command is under development.
    Please re-run this command regularly to see if your track passes the latest linting rules.

    Missing file:
    /tmp/foo/config.json

    SIGSEGV: Illegal storage access. (Attempt to read from nil?)

With this commit, we get:

    The lint command is under development.
    Please re-run this command regularly to see if your track passes the latest linting rules.

    Missing file:
    /tmp/foo/config.json

    Missing directory:
    /tmp/foo/exercises/shared/.docs

    Missing directory:
    /tmp/foo/docs

    Configlet detected at least one problem.
    For more information on resolving the problems, please see the documentation:
    https://github.com/exercism/docs/blob/main/building/configlet/lint.md

The problem was introduced by commit e732894895f8, and does not occur in
releases before 4.0.0-alpha.31.

src/lint/track_config.nim
@@ -720,4 +720,5 @@ proc isTrackConfigValid*(trackDir: Path): bool =
     let trackConfigContents = readFile(trackConfigPath)
     result = satisfiesSecondPass(trackConfigContents, trackConfigPath)
 
-  checkExerciseDirsAreInTrackConfig(trackDir, j, result, trackConfigPath)
+  if j != nil:
+    checkExerciseDirsAreInTrackConfig(trackDir, j, result, trackConfigPath)