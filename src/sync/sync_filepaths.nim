ebc02361c0c63b5edc71a66f044bb620525d66fa
ee7 <45465154+ee7@users.noreply.github.com>
2021-11-06 13:00:03 +0100

    sync_filepaths: refactor `isSynced`
    This makes the conditions more consistent.

--- src/sync/sync_filepaths.nim
+++ src/sync/sync_filepaths.nim
@@ -90,13 +90,13 @@ func isSynced(f: ConceptExerciseFiles | PracticeExerciseFiles,
               patterns: FilePatterns): bool =
   # Returns `true` if every field of `f` is either non-empty or cannot be synced
   # from the corresponding field in `patterns`.
-  when f is ConceptExerciseFiles:
-    if patterns.exemplar.len > 0 and (f.exemplar.len == 0 or f.exemplar == [""]):
-      return false
-  when f is PracticeExerciseFiles:
-    if patterns.example.len > 0 and (f.example.len == 0 or f.example == [""]):
-      return false
-  (patterns.solution.len == 0 or (f.solution.len > 0 and f.solution != [""])) and
+  let cond =
+    when f is ConceptExerciseFiles:
+      patterns.exemplar.len == 0 or (f.exemplar.len > 0 and f.exemplar != [""])
+    else:
+      patterns.example.len == 0 or (f.example.len > 0 and f.example != [""])
+  cond and
+      (patterns.solution.len == 0 or (f.solution.len > 0 and f.solution != [""])) and
       (patterns.test.len == 0 or (f.test.len > 0 and f.test != [""])) and
       (patterns.editor.len == 0 or (f.editor.len > 0 and f.editor != [""]))
 
