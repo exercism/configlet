d7b5178e3155dd5f337d799e5063d3cc481d332b
ee7 <45465154+ee7@users.noreply.github.com>
2022-08-21 11:22:34 +0200

    completions(bash, fish): tweak formatting (#650)
    Currently, the completions aren't auto-generated from a Nim source of
    truth. Make it easier to scan the fish completions by making them more
    similar to the output of `configlet --help`:
    
    - Group `__fish_use_subcommand` completions.
    
    - Order subcommands and options alphabetically.
    
    - Align `-s`, `-l`, and `-d`.
    
    - Always put `-xa` on its own line.
    
    Also, format the bash completions with shfmt [1]:
    
        shfmt --indent 2 --case-indent --space-redirects --keep-padding
    
    [1] https://github.com/mvdan/sh/blob/v3.5.1/cmd/shfmt/shfmt.1.scd

--- completions/configlet.fish
+++ completions/configlet.fish
@@ -5,45 +5,49 @@ end
 # disable file completions everywhere
 complete -c configlet -f
 
-# global options
-complete -c configlet -s h -l help -d "Show help"
-complete -c configlet      -l version -d "Show version info"
-complete -c configlet -s t -l track-dir -d "Select a track directory" \
-  -x -a "(__fish_complete_directories)"
-complete -c configlet -s v -l verbosity -x -a "quiet normal detailed" -d "Verbose level"
-
 # subcommands with no options
-complete -c configlet -n "__fish_use_subcommand" -a lint -d "Check the track configuration for correctness"
-complete -c configlet -n "__fish_use_subcommand" -a generate -d "Generate concept exercise introductions"
+complete -c configlet -n "__fish_use_subcommand" -a generate   -d "Generate concept exercise introductions"
+complete -c configlet -n "__fish_use_subcommand" -a lint       -d "Check the track configuration for correctness"
+
+# subcommands with options
+complete -c configlet -n "__fish_use_subcommand" -a completion -d "Output a completion script for a given shell"
+complete -c configlet -n "__fish_use_subcommand" -a fmt        -d "Format the exercise '.meta/config.json' files"
+complete -c configlet -n "__fish_use_subcommand" -a info       -d "Track info"
+complete -c configlet -n "__fish_use_subcommand" -a sync       -d "Check or update Practice Exercise docs, metadata, and tests"
+complete -c configlet -n "__fish_use_subcommand" -a uuid       -d "Output a UUID"
 
 # completion subcommand
-complete -c configlet -n "__fish_use_subcommand" -a completion -d "Output a completion script for a given shell"
-complete -c configlet -n "__fish_seen_subcommand_from completion" -s s -l shell -d "Shell type" \
-  -x -a "bash fish zsh"
-
-# info subcommand
-complete -c configlet -n "__fish_use_subcommand" -a info -d "Track info"
-complete -c configlet -n "__fish_seen_subcommand_from info" -s o -l offline -d "Do not update prob-specs cache"
-
-# uuid subcommand
-complete -c configlet -n "__fish_use_subcommand" -a uuid -d "Output a UUID"
-complete -c configlet -n "__fish_seen_subcommand_from uuid" -s n -l num -x -d "How many UUIDs"
+complete -c configlet -n "__fish_seen_subcommand_from completion" -s s -l shell     -d "Shell type" \
+  -xa "bash fish zsh"
 
 # fmt subcommand
-complete -c configlet -n "__fish_use_subcommand" -a fmt -d "Format the exercise '.meta/config.json' files"
-complete -c configlet -n "__fish_seen_subcommand_from fmt" -s u -l update -d "Write changes"
-complete -c configlet -n "__fish_seen_subcommand_from fmt" -s y -l yes -d "Auto-confirm update"
-complete -c configlet -n "__fish_seen_subcommand_from fmt" -s e -l exercise -d "exercise slug" \
-  -x -a '(__fish_configlet_find_dirs ./exercises/{concept,practice})'
+complete -c configlet -n "__fish_seen_subcommand_from fmt"        -s e -l exercise  -d "exercise slug" \
+  -xa '(__fish_configlet_find_dirs ./exercises/{concept,practice})'
+complete -c configlet -n "__fish_seen_subcommand_from fmt"        -s u -l update    -d "Write changes"
+complete -c configlet -n "__fish_seen_subcommand_from fmt"        -s y -l yes       -d "Auto-confirm update"
+
+# info subcommand
+complete -c configlet -n "__fish_seen_subcommand_from info"       -s o -l offline   -d "Do not update prob-specs cache"
 
 # sync subcommand
-complete -c configlet -n "__fish_use_subcommand" -a sync -d "Check or update Practice Exercise docs, metadata, and tests"
-complete -c configlet -n "__fish_seen_subcommand_from sync" -s o -l offline -d "Do not update prob-specs cache"
-complete -c configlet -n "__fish_seen_subcommand_from sync" -s u -l update -d "Write changes"
-complete -c configlet -n "__fish_seen_subcommand_from sync" -s y -l yes -d "Auto-confirm update"
-complete -c configlet -n "__fish_seen_subcommand_from sync"      -l docs -d "Sync docs only"
-complete -c configlet -n "__fish_seen_subcommand_from sync"      -l filepaths -d 'Populate .meta/config.json "files" entry'
-complete -c configlet -n "__fish_seen_subcommand_from sync"      -l metadata -d "Sync metadata only"
-complete -c configlet -n "__fish_seen_subcommand_from sync"      -l tests -d "For auto-confirming" -x -a "choose include exclude"
-complete -c configlet -n "__fish_seen_subcommand_from sync" -s e -l exercise -d "exercise slug" \
-  -x -a '(__fish_configlet_find_dirs ./exercises/practice)'
+complete -c configlet -n "__fish_seen_subcommand_from sync"       -s e -l exercise  -d "exercise slug" \
+  -xa '(__fish_configlet_find_dirs ./exercises/practice)'
+complete -c configlet -n "__fish_seen_subcommand_from sync"       -s o -l offline   -d "Do not update prob-specs cache"
+complete -c configlet -n "__fish_seen_subcommand_from sync"       -s u -l update    -d "Write changes"
+complete -c configlet -n "__fish_seen_subcommand_from sync"       -s y -l yes       -d "Auto-confirm update"
+complete -c configlet -n "__fish_seen_subcommand_from sync"            -l docs      -d "Sync docs only"
+complete -c configlet -n "__fish_seen_subcommand_from sync"            -l filepaths -d 'Populate .meta/config.json "files" entry'
+complete -c configlet -n "__fish_seen_subcommand_from sync"            -l metadata  -d "Sync metadata only"
+complete -c configlet -n "__fish_seen_subcommand_from sync"            -l tests     -d "For auto-confirming" \
+  -xa "choose include exclude"
+
+# uuid subcommand
+complete -c configlet -n "__fish_seen_subcommand_from uuid"       -s n -l num       -d "How many UUIDs" -x
+
+# global options
+complete -c configlet                                             -s h -l help      -d "Show help"
+complete -c configlet                                                  -l version   -d "Show version info"
+complete -c configlet                                             -s t -l track-dir -d "Select a track directory" \
+  -xa "(__fish_complete_directories)"
+complete -c configlet                                             -s v -l verbosity -d "Verbose level" \
+  -xa "quiet normal detailed"
