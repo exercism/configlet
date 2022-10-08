b4f876cf2748eeab6bdd922c841c6a8c692ec230
ee7 <45465154+ee7@users.noreply.github.com>
2022-10-08 11:38:02 +0200

    tests(all_tests): find via `walkDir`, not `git ls-files`
    This is somewhat simpler, but may become trickier if we add a
    subdirectory that contains test files (we'd need to add some
    `if kind == pcDir` condition).
    
    One advantage of the `git ls-files` approach was that it automatically
    avoided finding test files in subdirectories for track repos that are
    created when running the integration tests.

--- tests/all_tests.nim
+++ tests/all_tests.nim
@@ -1,24 +1,23 @@
-import std/[macros, os, sequtils, strutils]
-from ../patches/patch import gorgeCheck
+import std/[algorithm, macros, os, strutils]
 
-proc getImports: NimNode =
-  const files = block:
-    const thisDir = currentSourcePath().parentDir()
-    const cmd = "git -C \"" & thisDir & "\" ls-files" # Don't match pattern here.
-    gorgeCheck(cmd, "command failed:\n" & cmd)
-  result = nnkBracket.newTree()
-  for f in files.splitLines():
-    if f.startsWith("test_") and f.endsWith(".nim"):
-      result.add ident(f[0..^5]) # Remove .nim file extension
-  expectMinLen(result, 9)
+proc getSortedTestFiles(dir: string): seq[string] =
+  result = @[]
+  # Note that we cannot use `walkFiles` or `walkPattern` at compile time.
+  for kind, path in walkDir(dir, relative = true):
+    if kind == pcFile:
+      if path.startsWith("test_") and path.endsWith(".nim"):
+        result.add path[0..^5] # Remove .nim file extension.
+  sort result
 
-macro importGitTrackedTestFiles =
-  ## Imports every module in the current directory that is both:
-  ## - tracked by `git`
-  ## - and has a filename that matches `test_*.nim`
-  let imports = getImports()
-  result = quote do:
-    import "."/`imports`
-  echo "Combining these git-tracked test files:\n" & imports.toSeq().join("\n")
+macro importTestFiles =
+  ## Imports every Nim module that begins with `test_` in the parent directory.
+  const files = currentSourcePath().parentDir().getSortedTestFiles()
+  var bracket = nnkBracket.newTree()
+  for f in files:
+    bracket.add ident(f)
+  expectMinLen(bracket, 9)
+  result = newStmtList(quote do:
+    import "."/`bracket`)
+  echo "all_tests: 'importTestFiles' produced:\n" & result.repr
 
-importGitTrackedTestFiles()
+importTestFiles()
