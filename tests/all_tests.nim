2a04e8b6f933a30bd2fc965f3371f0f7dc44c63f
ee7 <45465154+ee7@users.noreply.github.com>
2022-10-18 09:39:02 +0200

    tests(all_tests), helpers: find test files at compile time (#672)
    Remove the need to maintain a list of test files in `all_tests.nim`.
    
    Recall that:
    - We have `all_tests.nim` because compiling that one file is currently
      much faster than compiling each test file separately.
    - We define `nimble test` to run `all_tests.nim`.
    
    Compiling the file shows the current result of the macro:
    
        all_tests: 'importTestFiles' produced:
    
        import
          "." /
              [test_binary, test_fmt, test_generate, test_json, test_lint,
               test_probspecs, test_sync, test_tracks, test_uuid]

--- tests/all_tests.nim
+++ tests/all_tests.nim
@@ -1,11 +1,19 @@
-import "."/[
-  test_binary,
-  test_fmt,
-  test_generate,
-  test_json,
-  test_lint,
-  test_probspecs,
-  test_sync,
-  test_tracks,
-  test_uuid,
-]
+import std/[macros, os]
+import "."/helpers
+
+proc genBracket: NimNode =
+  const thisDir = currentSourcePath().parentDir().Path()
+  const files = thisDir.getSortedFiles(relative = true, "test_$[w].nim$.")
+  result = nnkBracket.newTree()
+  for f in files:
+    result.add ident(f.string[0..^5]) # Remove .nim file extension.
+  expectMinLen(result, 9)
+
+macro importTestFiles =
+  ## Imports every Nim module that begins with `test_` in the parent directory.
+  let bracket = genBracket()
+  result = newStmtList(quote do:
+    import "."/`bracket`)
+  echo "all_tests: 'importTestFiles' produced:\n" & result.repr
+
+importTestFiles()
