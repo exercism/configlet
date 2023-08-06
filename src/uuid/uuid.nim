1f16cdfec7c51161ca631a9a5693b2d397c7b19b
ee7 <45465154+ee7@users.noreply.github.com>
2023-08-06 20:00:05 +0200

    uuid: avoid object
    Arguable readability improvement: remove the need to write `.bytes`. If
    the object was exported, it's not obvious that at a callsite there's
    only one field.
    
    There's no performance difference. A Nim object has no bloat - it was
    still 16 bytes before.
    
    This could be a distinct array, but borrowing the `urandom` proc and
    slice operator is verbose.

--- src/uuid/uuid.nim
+++ src/uuid/uuid.nim
@@ -2,15 +2,13 @@ import std/[strformat, sysrand]
 import ".."/logger
 
 type
-  Uuid = object
-    bytes: array[16, byte]
+  Uuid = array[16, byte]
 
 proc genUuid*: Uuid =
   ## Returns a version 4 UUID, using the system CSPRNG as the source of randomness.
-  result = Uuid()
-  if urandom(result.bytes):
-    result.bytes[6] = (result.bytes[6] and 0x0f) or 0x40 # Set version to 4
-    result.bytes[8] = (result.bytes[8] and 0x3f) or 0x80 # Set variant to 1
+  if urandom(result):
+    result[6] = (result[6] and 0x0f) or 0x40 # Set version to 4
+    result[8] = (result[8] and 0x3f) or 0x80 # Set variant to 1
   else:
     stderr.writeLine "uuid: error: failed to generate UUID"
     quit 1
@@ -24,8 +22,8 @@ func `$`*(u: Uuid): string =
   result[23] = '-'
   for i, j in [0, 2, 4, 6, 9, 11, 14, 16, 19, 21, 24, 26, 28, 30, 32, 34]:
     const hex = "0123456789abcdef"
-    result[j + 0] = hex[u.bytes[i] shr 4]
-    result[j + 1] = hex[u.bytes[i] and 0x0f]
+    result[j + 0] = hex[u[i] shr 4]
+    result[j + 1] = hex[u[i] and 0x0f]
 
 proc outputUuids(n: Positive) =
   ## Writes `n` version 4 UUIDs to stdout. Writes only 1000 UUIDs if `n` is
