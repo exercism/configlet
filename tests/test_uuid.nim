005ff35ef5f550aec0622e6f6e1deb443666f904
ee7 <45465154+ee7@users.noreply.github.com>
2023-08-06 20:00:02 +0200

    tests(uuid): test for distinctness; refactor

--- tests/test_uuid.nim
+++ tests/test_uuid.nim
@@ -1,12 +1,19 @@
-import std/unittest
+import std/[sets, strformat, strutils, unittest]
 import "."/[lint/validators, uuid/uuid]
 
 proc main =
-  suite "genUUID: returns a string that isUuidV4 says is valid":
-    test "1000 UUIDs":
-      for i in 1 .. 1000:
+  suite "genUUID":
+    const n = 1_000
+    var uuids = initHashSet[string](n)
+
+    test &"can generate {insertSep($n)} valid version 4 UUIDs":
+      for i in 1 .. n:
         let uuid = $genUuid()
+        uuids.incl uuid
         check isUuidV4(uuid)
+
+    test "those UUIDs are distinct":
+      check uuids.len == n
 
 main()
 {.used.}
