17309ce85b879c8936c2c575bcd4984bc39cb75d
Erik Schierboom <erik_schierboom@hotmail.com>
2023-10-06 09:56:58 +0200

    Extract binary helpers

--- tests/test_binary.nim
+++ tests/test_binary.nim
@@ -1,42 +1,6 @@
 import std/[os, osproc, strformat, strscans, strutils, unittest]
 import exec, helpers, lint/validators, sync/probspecs
-
-const
-  testsDir = currentSourcePath().parentDir()
-  repoRootDir = testsDir.parentDir()
-
-template execAndCheckExitCode(expectedExitCode: int; cmd: string;
-                              inputStr = "") =
-  ## Runs `cmd`, supplying `inputStr` on stdin, and checks that its exit code is
-  ## `expectedExitCode`
-  let exitCode = execCmdEx(cmd, input = inputStr)[1]
-  check:
-    exitCode == expectedExitCode
-
-template execAndCheck(expectedExitCode: int; cmd, expectedOutput: string;
-                      inputStr = "") =
-  ## Runs `cmd`, supplying `inputStr` on stdin, and checks that:
-  ## - its exit code is `expectedExitCode`
-  ## - its output is `expectedOutput`
-  let (outp, exitCode) = execCmdEx(cmd, input = inputStr)
-  check:
-    exitCode == expectedExitCode
-    outp == expectedOutput
-
-template gitRestore(dir, arg: string) =
-  let args = ["-C", dir, "restore", arg]
-  check git(args).exitCode == 0
-
-template testDiffThenRestore(dir, expectedDiff, restoreArg: string) =
-  ## Runs `git diff` in `dir`, and tests that the output is `expectedDiff`. Then
-  ## runs `git restore` with the argument `restoreArg`.
-  test "the diff is as expected":
-    let diff = gitDiffConcise(trackDir)
-    check diff == expectedDiff
-  gitRestore(dir, restoreArg)
-
-template checkNoDiff(trackDir: string) =
-  check gitDiffExitCode(trackDir) == 0
+import "."/[binary_helpers]
 
 proc testsForSync(binaryPath: static string) =
   const psDir = getCacheDir() / "exercism" / "configlet" / "problem-specifications"
