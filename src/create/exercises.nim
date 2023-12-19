9406ba238b8e4d9c4c401ce19c0fe190d1fc78c9
Erik Schierboom <erik_schierboom@hotmail.com>
2023-12-19 13:41:02 +0100

    initial work to update track config

--- src/create/exercises.nim
+++ src/create/exercises.nim
@@ -1,9 +1,41 @@
-import std/[os, strformat]
-import ".."/[cli, helpers, sync/sync, types_track_config]
-import "."/[approaches, articles]
+import std/[sets, os, strformat]
+import ".."/[cli, helpers, fmt/track_config, sync/sync, types_track_config, uuid/uuid]
 
 proc createConceptExercise*(conf: Conf) =
   echo "create ce"
 
 proc createPracticeExercise*(conf: Conf) =
-  echo "create pe"
+  let trackConfigPath = conf.trackDir / "config.json"
+  let trackConfig = parseFile(trackConfigPath, TrackConfig)
+  let trackExerciseSlugs = getSlugs(trackConfig.exercises, conf, trackConfigPath)
+  let userExercise = Slug(conf.action.exerciseCreate)
+
+  if userExercise in trackExerciseSlugs.`concept`:
+    let msg = &"There already is a concept exercise with `{userExercise}` " &
+              &"in the track config:\n{trackConfigPath}"
+    stderr.writeLine msg
+    quit 1
+  elif userExercise in trackExerciseSlugs.practice:
+    let msg = &"There already is a practice exercise with `{userExercise}` " &
+              &"in the track config:\n{trackConfigPath}"
+    stderr.writeLine msg
+    quit 1
+
+  let exerciseDir = conf.trackDir / "exercises" / "practice" / $userExercise
+  let exercise = PracticeExercise(
+    slug: userExercise,
+    name: $userExercise,
+    uuid: $genUuid(),
+    practices: OrderedSet[string](),
+    prerequisites: OrderedSet[string](),
+    difficulty: 1,
+    status: sMissing
+  )
+
+  var a = trackConfig.exercises.practice
+  a.add(exercise)
+
+  trackConfig.exercises.practice = a;
+
+  let prettied = prettyTrackConfig(trackConfig)
+  writeFile(trackConfigPath, prettied)
