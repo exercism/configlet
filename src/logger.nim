0d3e4073fdae2b85796d709baa97d191a242863a
Erik Schierboom <erik_schierboom@hotmail.com>
2024-01-11 15:29:48 +0100

    logger: add withLevel template (#849)
    Allow running code with specified verbosity, which will be useful later.

--- src/logger.nim
+++ src/logger.nim
@@ -1,16 +1,27 @@
 import std/logging
 import "."/cli
 
+let consoleLogger = newConsoleLogger(levelThreshold = lvlNotice,
+                                     fmtStr = "")
+
 func toLevel(verbosity: Verbosity): Level =
   case verbosity
   of verQuiet: lvlNone
   of verNormal: lvlNotice
   of verDetailed: lvlInfo
 
+proc setLevel(verbosity: Verbosity) =
+  consoleLogger.levelThreshold = toLevel(verbosity)
+
 proc setupLogging*(verbosity: Verbosity) =
-  let consoleLogger = newConsoleLogger(levelThreshold = toLevel(verbosity),
-                                       fmtStr = "")
   addHandler(consoleLogger)
+  setLevel(verbosity)
+
+template withLevel*(verbosity: Verbosity, body: untyped): untyped =
+  let currentLevel = consoleLogger.levelThreshold
+  consoleLogger.levelThreshold = toLevel(verbosity)
+  body
+  consoleLogger.levelThreshold = currentLevel
 
 template logNormal*(msgs: varargs[string]) =
   notice(msgs)
