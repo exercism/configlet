21998f07902a98c9e3122fc562d8a1ca3a45925d
Erik Schierboom <erik_schierboom@hotmail.com>
2024-01-11 12:27:59 +0100

    Only sync once

--- src/sync/probspecs.nim
+++ src/sync/probspecs.nim
@@ -21,6 +21,9 @@ proc `[]`(testCase: ProbSpecsTestCase, name: string): JsonNode {.borrow.}
 proc hasKey(testCase: ProbSpecsTestCase, key: string): bool {.borrow.}
 proc pretty*(testCase: ProbSpecsTestCase, indent = 2): string {.borrow.}
 
+# Keeps track if we already ran `git fetch` before
+var fetched = false;
+
 func canonicalDataFile(probSpecsExerciseDir: ProbSpecsExerciseDir): string =
   probSpecsExerciseDir / "canonical-data.json"
 
@@ -156,28 +159,30 @@ proc validate(probSpecsDir: ProbSpecsDir, conf: Conf) =
       const upstreamLocation = "exercism/problem-specifications"
       let remoteName = getNameOfRemote(probSpecsDir, upstreamHost, upstreamLocation)
 
-      # `fetch` and `merge` separately, for better error messages.
-      logNormal(&"Updating cached 'problem-specifications' data...")
-      try:
-        discard gitCheck(0, ["fetch", "--quiet", remoteName, mainBranchName],
-                        &"failed to fetch '{mainBranchName}' in " &
-                        &"problem-specifications directory: '{probSpecsDir}'")
-      except OSError:
-        const msg = """
-          Unable to update the problem-specifications cache.
+      if not fetched:
+        # `fetch` and `merge` separately, for better error messages.
+        logNormal(&"Updating cached 'problem-specifications' data...")
+        try:
+          discard gitCheck(0, ["fetch", "--quiet", remoteName, mainBranchName],
+                          &"failed to fetch '{mainBranchName}' in " &
+                          &"problem-specifications directory: '{probSpecsDir}'")
+          fetched = true
+        except OSError:
+          const msg = """
+            Unable to update the problem-specifications cache.
 
-          You can either:
+            You can either:
 
-          - ensure that you have network connectivity, and run the same configlet command again
-          - or add the '--offline' option to skip updating the cache
+            - ensure that you have network connectivity, and run the same configlet command again
+            - or add the '--offline' option to skip updating the cache
 
-          The most recent commit in the problem-specifications cache is:
+            The most recent commit in the problem-specifications cache is:
 
-          """.unindent()
-        const format = "commit %H%nAuthor:     %an <%ae>%nCommitDate: %cD%n%n    %s"
-        let mostRecentCommit = gitCheck(0, ["log", "-n1",
-                                            &"--format={format}"]).strip().indent(4)
-        showError(msg & mostRecentCommit, writeHelp = false)
+            """.unindent()
+          const format = "commit %H%nAuthor:     %an <%ae>%nCommitDate: %cD%n%n    %s"
+          let mostRecentCommit = gitCheck(0, ["log", "-n1",
+                                              &"--format={format}"]).strip().indent(4)
+          showError(msg & mostRecentCommit, writeHelp = false)
 
       discard gitCheck(0, ["merge", "--ff-only", &"{remoteName}/{mainBranchName}"],
                        &"failed to merge '{mainBranchName}' in " &
